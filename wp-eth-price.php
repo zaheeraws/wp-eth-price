208dbc8b3d80fe95c852bccae53cfa9dbf4d8526
zaheer <support@fieztech.com>
2018-10-03 14:25:26 +0500

    just for electroneumprice.today

--- wp-eth-price.php
+++ wp-eth-price.php
@@ -13,7 +13,7 @@
  * @package           Wp_Eth_Price
  *
  * @wordpress-plugin
- * Plugin Name:       Ethereum Price
+ * Plugin Name:       Crypto Coin Price
  * Plugin URI:        https://github.com/zaheeraws/wp-eth-price.git
  * Description:       You can use it in your post by calling shortcode. [eth_price coin="ETN" usd="1" eur="1"] put 0 instead of 1 to disable pricing for that currency. Write the coin short code and done. Total 3098 coins supported. see coin codes here: https://github.com/zaheeraws/wp-eth-price/blob/master/README.txt
  * Version:           1.0.0
@@ -26,8 +26,8 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+    die;
 }
 
 /**
@@ -35,34 +35,36 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define('PLUGIN_NAME_VERSION', '1.0.0');
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-wp-eth-price-activator.php
  */
-function activate_wp_eth_price() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-eth-price-activator.php';
-	Wp_Eth_Price_Activator::activate();
+function activate_wp_eth_price()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-wp-eth-price-activator.php';
+    Wp_Eth_Price_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-wp-eth-price-deactivator.php
  */
-function deactivate_wp_eth_price() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-eth-price-deactivator.php';
-	Wp_Eth_Price_Deactivator::deactivate();
+function deactivate_wp_eth_price()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-wp-eth-price-deactivator.php';
+    Wp_Eth_Price_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_wp_eth_price' );
-register_deactivation_hook( __FILE__, 'deactivate_wp_eth_price' );
+register_activation_hook(__FILE__, 'activate_wp_eth_price');
+register_deactivation_hook(__FILE__, 'deactivate_wp_eth_price');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-wp-eth-price.php';
+require plugin_dir_path(__FILE__) . 'includes/class-wp-eth-price.php';
 
 /**
  * Begins execution of the plugin.
@@ -73,36 +75,29 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-wp-eth-price.php';
  *
  * @since    1.0.0
  */
-function run_wp_eth_price() {
+function run_wp_eth_price()
+{
 
-	$plugin = new Wp_Eth_Price();
-	$plugin->run();
+    $plugin = new Wp_Eth_Price();
+    $plugin->run();
 
 }
 run_wp_eth_price();
 
-function ft_eth_price($atts = [], $content = null, $tag = ''){
-	
-	// normalize attribute keys, lowercase
-    $atts = array_change_key_case((array)$atts, CASE_LOWER);
-    $coin = "ETN";
-    if( isset($atts['coin']) && $atts['coin'] != "" ){
-    	$coin = $atts['coin'];
-    }
-	$json_data = file_get_contents("https://min-api.cryptocompare.com/data/pricemulti?fsyms={$coin}&tsyms=USD,EUR");
-	$a = json_decode($json_data);
+function ft_eth_price($atts = [], $content = null, $tag = '')
+{
 
-    $usd = "";
-    if( isset($atts['usd']) && $atts['usd'] == 1 ){
-    	$usd = "<h3>$ {$a->{$coin}->USD}</h3>";
+    // normalize attribute keys, lowercase
+    $atts = array_change_key_case((array) $atts, CASE_LOWER);
+    $coin = "ETN";
+    if (isset($atts['coin']) && $atts['coin'] != "") {
+        $coin = $atts['coin'];
     }
-    $ur = "";
-    if( isset($atts['eur']) && $atts['eur'] == 1 ){
-    	$ur = "€{$a->{$coin}->EUR}</h3>";
-    }
-	echo "{$usd}<br/><h3>{$ur}";
+    $json_data = file_get_contents("https://min-api.cryptocompare.com/data/pricemultifull?fsyms={$coin}&tsyms=USD,EUR");
+    //https://min-api.cryptocompare.com/data/pricemulti?fsyms={$coin}&tsyms=USD,EUR
+    $a = json_decode($json_data)->DISPLAY;
+
+    require_once plugin_dir_path(__FILE__) .'./public/partials/plugin-view.php';
 }
 
-add_shortcode('eth_price', 'ft_eth_price');
-
-
+add_shortcode('eth_price_title', 'ft_eth_price');
